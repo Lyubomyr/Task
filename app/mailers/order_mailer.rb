034f2d240772ba18dc29b36a604bf3eca096d181
Lyubomyr <fedorniakl@gmail.com>
2012-11-29 12:13:10 +0200

    fix mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -8,12 +8,12 @@ class OrderMailer < ActionMailer::Base
   #
   def picture_order_created(picture_order)
     	@picture_order = picture_order
-  	mail to: "fedorniakl@gmail.com", cc: "brusd.com@gmail.com",  subject: "New Picture Order"
+  	mail to: "fedorniakl@gmail.com", cc: "brusd.com@gmail.com",  subject: I18n.t("picture_order_created.subject")
   end
-
+   :subject => I18n.t("new_message.subject", :title => @topic.subject)
   def picture_order_created_user(picture_order)
     	@picture_order = picture_order
-  	mail to: picture_order.user_email,  subject: "Carpe Diem - Order Made!"
+  	mail to: picture_order.user_email,  subject: I18n.t("picture_order_created_user.subject")
   end
 
 end
