3d1f4111af1b8e84840c3e78b3ac19177938a336
Lyubomyr <fedorniakl@gmail.com>
2012-11-29 12:18:19 +0200

    fix mailer and add new logo

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -10,7 +10,7 @@ class OrderMailer < ActionMailer::Base
     	@picture_order = picture_order
   	mail to: "fedorniakl@gmail.com", cc: "brusd.com@gmail.com",  subject: I18n.t("picture_order_created.subject")
   end
-   :subject => I18n.t("new_message.subject", :title => @topic.subject)
+
   def picture_order_created_user(picture_order)
     	@picture_order = picture_order
   	mail to: picture_order.user_email,  subject: I18n.t("picture_order_created_user.subject")
